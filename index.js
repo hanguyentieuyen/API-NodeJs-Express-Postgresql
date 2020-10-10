d4ef1660b4f551bc3a53e273e9ab0d91dae8d528
yen <hanguyentieuyen@gmail.com>
2020-10-10 12:01:42 +0700

    fix PORT

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const db = require('./queries')
-const port = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json())
 app.use(
