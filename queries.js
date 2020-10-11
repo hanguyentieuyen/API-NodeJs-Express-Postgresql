008e41e5d005ec7fbfa1034bd93a1cc8c1c5b52c
yen <hanguyentieuyen@gmail.com>
2020-10-11 21:57:47 +0700

    test

--- queries.js
+++ queries.js
@@ -8,9 +8,9 @@ const pool = new Pool({
 })
 const getUsers = (request, response) => {
    pool.query('SELECT * FROM users ORDER BY id ASC', (results) => {
-  //   if (error) {
-  //     throw error
-  //   }
+    if (error) {
+      throw error
+    }
     response.status(200).json(results.rows)
   })
 }
